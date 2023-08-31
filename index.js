193e28038cb5a37722e7f8fc941f38347de7b790
Nazeerkhan1234 <nazeerkhan9695@gmail.com>
2023-08-31 10:26:25 +0530

    express routes

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hi")
+const express = require('express');
+const app = express();
+// Named Import
+const {adminRoute }= require('./src/routes/admin/adminRoute');
+const { deliveryboyRoute } = require('./src/routes/deliveryboy/deliveryBoyRoutes');
+app.use('/customer',adminRoute);
+app.use('/deliveryboy',deliveryboyRoute);
+let port = 4000;
+app.listen(port,()=>{
+    console.log('The server is listening on',port);
+});
---
+++ src/routes/admin/adminRoute.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const adminRoute = express.Router()
+//Lets define some routes
+adminRoute.get('/getproducts',(req,res)=>{
+    res.status(200).json({
+        msg:"ok"
+    });
+});
+adminRoute.get('/getorders',(req,res)=>{
+    res.status(200).json({
+        msg:"ok2"
+    });
+});
+//Named Export
+module.exports = {adminRoute:adminRoute}
